1f79ca330cbb5d24a18823755b493107fce0b462
HerrBergenholtz <zbdv8800@gmail.com>
2024-10-22 15:37:04 +0200

    Add project files.

---
+++ Uppgift-TryParse/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Uppgift_TryParse
+{
+    class Program
+    {
+        static void Main()
+        {
+            int resultat = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Skriv en siffra");
+                if (!TryParse(Console.ReadLine(), out resultat))
+                {
+                    Console.WriteLine("FEL! Skriv in en siffra");
+                }
+                else
+                {
+                    Console.WriteLine(resultat);
+                }
+            }
+        }
+
+        public static bool TryParse(string input, out int resultat)
+        {
+            try
+            {
+                resultat = int.Parse(input);
+                return true;
+            }
+            catch (FormatException)
+            {
+                resultat = 0;
+                return false;
+            }
+        }
+    }
+}
